89db8006bdab19d394976005b6c15bacbce160aa
asdas <45>
2018-12-20 14:43:08 +0800

    lab

---
+++ app/src/main/java/com/example/user/lab12/Data.java
@@ -0,0 +1,14 @@
+package com.example.user.lab12;
+
+class Data {
+    Result result;
+    class Result{
+        Results[] results;
+
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+
+}
